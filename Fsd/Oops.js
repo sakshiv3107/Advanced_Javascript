62a084a042416ad9bd028771bc221f6298e00ade
sakshiv3107 <sakshi.vishnoi3107@gmail.com>
2025-04-23 15:24:54 +0530

    Oops completed

--- Fsd/Oops.js
+++ Fsd/Oops.js
@@ -42,14 +42,135 @@
 //Methods
 //Functions on a class are called methods.
 
-class Person{
-        constructor(firstname,lastname){
-            this.firstname=firstname;
-            this.lastname=lastname;
-        }
-        greet(){
-            console.log("Hello",this.firstname,this.lastname);
-        }
-    }
-    let p=new Person("Sakshi","Vishnoi");
-    p.greet(); //calling method greet on object p
+// class Person{
+//         constructor(firstname,lastname){
+//             this.firstname=firstname;
+//             this.lastname=lastname;
+//         }
+//         greet(){
+//             console.log("Hello",this.firstname,this.lastname);
+//         }
+//     }
+//     let p=new Person("Sakshi","Vishnoi");
+//     p.greet(); //calling method greet on object p
+
+    // class Person{
+        //     constructor(firstname,lastname){
+        //         this.firstname=firstname;
+        //         this.lastname=lastname;
+        //     }
+        //     greet(){
+        //         console.log("Hi there!");
+        //     }
+        //     compliment(name,object){
+        //         return "That's wonderful "+ object +", "+name;
+        //     }
+        // }
+        //     let p =new Person("Saloni","Singh");
+        //     let compliment=p.compliment("Harry","hat");
+        //     console.log(compliment);
+        
+        //Properties sometimes aslso called fields hold the data of the class.
+        
+        // class Person{
+        //     #firstname;
+        //     #lastname;
+        //     constructor(firstname,lastname){
+        //         this.#firstname=firstname;
+        //         this.#lastname=lastname;
+        //     }
+        // }
+        // let p=new Person("Saloni","Singh");
+        // console.log(p.firstname);
+        
+        // class Person{
+        //     #firstname;
+        //     #lastname;
+        //     constructor(firstname,lastname){
+        //         if(firstname.startsWith("S")){
+        //         this.#firstname=firstname;
+        //         }
+        //         else{
+        //             this.#firstname="S"+firstname;
+        //         }
+        //         this.#lastname=lastname;
+        //     }
+        // }
+        // let p=new Person("Saloni","Singh");
+        // console.log(p.firstname);
+        
+        // class Person{
+        //     #firstname;
+        //     #lastname;
+            // constructor(firstname,lastname){
+            //     this.#firstname=firstname;
+            //     this.#lastname=lastname;
+            // }
+            // get firstname(){
+            //     return this.#firstname;
+            // }
+            // set firstname(firstname){
+            //     this.#firstname=firstname;
+            // }
+            // get lastname(){
+            //     return this.#lastname;
+        //     }
+        //     set lastname(lastname){
+        //         this.#lastname=lastname;
+        //     }
+        
+        // }
+        // let p=new Person("Saloni","Singh");
+        // console.log(p.firstname);
+        // p.firstname="Ananya";
+        // p.lastname="Singh";
+        // console.log(p.firstname);
+    // }
+
+    //Inhertiance
+
+    // class Vehicle{
+    //     constructor(color,currentSpeed,maxSpeed){
+    //         this.color=color;
+    //         this.currentSpeed=currentSpeed;
+    //         this.maxSpeed=maxSpeed;
+    //     }
+    //     move(){
+    //         console.log("Moving at speed",this.currentSpeed,"km/h");
+    //     }
+    //     accelerate(amount){
+    //         this.currentSpeed+=amount;
+    //     }
+    // }
+    // class motorcycle extends Vehicle{
+    //     constructor(color,currentSpeed,maxSpeed,fuel){
+    //         super(color,currentSpeed,maxSpeed);
+    //         this.fuel=fuel;
+    //     }
+    //     doWheelie(){
+    //         console.log("Doing on one wheel!");
+    //     }
+    // }    
+    // let motor=new motorcycle("red",0,200,"petrol",);
+    // console.log(motor.color);
+    // motor.accelerate(50);
+    // motor.move();
+
+    //Prototypes
+    class Person{
+                constructor(firstname,lastname){
+                    this.firstname=firstname;
+                    this.lastname=lastname;
+                }
+                greet(){
+                    console.log("Hello",this.firstname,this.lastname);
+                }
+            }
+            Person.prototype.introduce=function(){
+                console.log("Hi, I'm",this.firstname,this.lastname);
+            }
+            Person.prototype.favouritecolor="blue";
+
+            let p=new Person("Sakshi","Vishnoi");
+            console.log(p.favouritecolor); 
+            p.introduce(); 
