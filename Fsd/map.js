281ea986507192173eac9f0f081e17e2305a9907
sakshiv3107 <sakshi.vishnoi3107@gmail.com>
2025-04-29 12:57:02 +0530

    map.js updation

--- Fsd/map.js
+++ Fsd/map.js
@@ -77,11 +77,75 @@
 
 //Alias
 
-const person={
-    firstname:"Saloni",
-    lastname:"Singh",
-    age:20
-};
-let{lastname:name}=person;
-console.log(name);
+// const person={
+//     firstname:"Saloni",
+//     lastname:"Singh",
+//     age:20
+// };
+// let{lastname:name}=person;
+// console.log(name);
 
+// let name="GurukulTheSchools";
+// let[a1,a2,a3,a4,a5]=name;
+// console.log(a1);
+// console.log(a2);
+// console.log(a3);    
+
+// const fruits=["banana","apple","orange"];
+// let[fruit1,fruit2]=fruits;
+// console.log(fruit1);
+// console.log(fruit2);
+
+// const fruits=["banana","apple","orange"];
+// let{[2]:fruit1,[1]:fruit2}=fruits;
+// console.log(fruit1);
+
+// const numbers=[1,2,3,4,5,6,7,8,9,10];
+// const[a,b,c,...rest]=numbers;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(rest);
+
+// const fruits=new Map([
+//     ['banana', 300],
+//     ['apple', 200],
+//     ['orange', 100],
+// ])
+// let text="";
+// for(const [key,value] of fruits){
+//     text+=key+"is "+value+" ";
+// }
+
+// let firstname="Saloni";
+// let lastname="Singh";
+// [firstname,lastname]=[lastname,firstname];
+// console.log(firstname);
+
+//Javascript Exponentiation(**)
+
+// let x=5;
+// let z=x**2;
+// console.log(z);
+// let y=2;
+// let z1=Math.pow(x,y);
+// console.log(z1);
+
+// let x=5;
+// x**=2;
+// console.log(x);
+
+// //array include()
+// const fruits=["banana","apple","orange"];
+// // fruits.includes("banana");
+// console.log(fruits.includes("banana"));
+
+// const arr=["one","two","three",,];
+// console.log(arr.length);
+
+// const person={
+//     firstname:"Saloni",
+//     lastname:"Singh",
+//     age:30,
+// }
+// console.log(person);
